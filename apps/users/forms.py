74fd13300dbd5bd1ba5eea31974bc5c514b7baf6
bin wu <ben.w.dbm@gmail.com>
2017-06-13 18:12:22 +0800

    user apps

---
+++ apps/users/forms.py
@@ -0,0 +1,21 @@
+'''
+Created on Jun 13, 2017
+
+@author: ben
+'''
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
+
+def is_member(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
+class AuthenticationFormWithAdminGroupOkay(AuthenticationForm):
+    def confirm_login_allowed(self, user):
+        if not is_member(user, 'admin'):
+            raise ValidationError(
+                _("This account dose not belong to admin group."),
+                code='group',
+            )
+    
