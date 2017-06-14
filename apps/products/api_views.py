1aad4eb9e0252f4ff56224b9769407327e95ae8c
taylor <taylor.h.dbm@gmail.com>
2017-06-14 15:01:02 +0800

    add product api

--- apps/products/api_views.py
+++ apps/products/api_views.py
@@ -1,7 +1,9 @@
-from .models import Products
+from .models import Products, ACTIVE_STATUS
 from rest_framework import generics, mixins
 from rest_framework.permissions import AllowAny
-from .serializers import ProductListSerializer
+from .serializers import ProductListSerializer, ProductDetailSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class ProductList(mixins.ListModelMixin,generics.GenericAPIView):
     #limit 8 softwares
@@ -13,3 +15,33 @@ class ProductList(mixins.ListModelMixin,generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
 
         return self.list(request, *args, **kwargs)
+
+class ProductListAll(mixins.ListModelMixin, generics.GenericAPIView):
+    """all product"""
+    queryset = Products.objects.filter(status=ACTIVE_STATUS).filter(in_homepage=True)
+    serializer_class = ProductListSerializer
+    permission_classes = [AllowAny,]
+
+
+    def get(self, request, *args, **kwargs):
+
+        return self.list(request, *args, **kwargs)
+
+
+class ProductDetail(generics.GenericAPIView):
+    """software detail"""
+    permission_classes = [AllowAny,]
+    serializer_class = ProductDetailSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            id = request.query_params.get('id', None)
+            result = generics.get_object_or_404(Products, uuid=id)
+        except Exception as e:
+            print(e)
+            return Response("error", status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(result,data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -11,3 +11,19 @@ class ProductListSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Products
         fields = ['id', 'software_pic', 'name', 'type', 'summary', 'features']
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    """software detail serialzer"""
+    features = serializers.JSONField(read_only=True)
+    environments = serializers.JSONField(read_only=True)
+    screenshots = serializers.JSONField(read_only=True,source='get_screenshots')
+    videos = serializers.JSONField(read_only=True,source='get_videos')
+
+    class Meta:
+        model = Products
+        fields = ['id', 'name', 'type','total_users', 'summary', 'description', 'product_url', 'product_pic',
+                  'product_img', 'latest_version', 'facebook_url', 'google_plus_url',
+                  'linkedin_url', 'twitter_url', 'document_url',  'free_plan', 'features', 'environments','screenshots',
+                  'videos']
+        read_only_fields = ('name','type','features','environments','screenshot')
