7d934bacd0f5f40108bd14ea7a69950cc9af255f
Claire Huang <claire.h.dbm@gmail.com>
2017-06-16 14:29:22 +0800

    Add package foreign key in orders model

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from common.utilities.utils import uuid_to_str
 from common.apps.products.models import Products, Plans
 from common.apps.clients.models import Clients
+from common.apps.packages.models import Packages
 
 
 ORDER_STATUS = (
@@ -17,6 +18,7 @@ class Orders(models.Model):
     client = models.ForeignKey(Clients,on_delete=models.PROTECT)
     product = models.ForeignKey(Products,on_delete=models.PROTECT)
     plan = models.ForeignKey(Plans,on_delete=models.PROTECT)
+    package = models.ForeignKey(Packages, models.SET_NULL, null=True, blank=True)
     amount = models.DecimalField('Total Fee', max_digits=10, decimal_places=2, null=True)
     status = models.CharField(max_length=32, choices=ORDER_STATUS)
     comment = models.TextField('Comment', max_length=2048, null=True, blank=True)
