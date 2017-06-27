a041b2ceb76bece51fee478642e14f57011d0b66
bin wu <ben.w.dbm@gmail.com>
2017-06-27 10:12:48 +0800

    test

--- apps/users/backends.py
+++ apps/users/backends.py
@@ -3,15 +3,17 @@ Created on Jun 23, 2017
 
 @author: ben
 '''
-from common.third_party.django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.backends import ModelBackend
 from common.apps.users.forms import is_member
 
 class AllowAdminGroupUsersModelBackend(ModelBackend):
     def user_can_authenticate(self, user):
-        is_superuser = getattr(user, 'is_superuser', None)
-        return is_member(user, 'admin') or is_superuser or is_superuser is None
+        #is_superuser = getattr(user, 'is_superuser', None)
+        #return is_member(user, 'admin') or is_superuser or is_superuser is None
+        return True
     
 class AllowStaffGroupUsersModelBackend(ModelBackend):
     def user_can_authenticate(self, user):
-        is_superuser = getattr(user, 'is_superuser', None)
-        return is_member(user, 'staff') or is_superuser or is_superuser is None
+        #is_superuser = getattr(user, 'is_superuser', None)
+        #return is_member(user, 'staff') or is_superuser or is_superuser is None
+        return True
