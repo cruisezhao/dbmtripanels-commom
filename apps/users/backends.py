f15afc511651a7fe5e3a53ed394ac64c4d020dc8
bin wu <ben.w.dbm@gmail.com>
2017-06-27 10:19:34 +0800

    test

--- apps/users/backends.py
+++ apps/users/backends.py
@@ -8,12 +8,14 @@ from common.apps.users.forms import is_member
 
 class AllowAdminGroupUsersModelBackend(ModelBackend):
     def user_can_authenticate(self, user):
-        #is_superuser = getattr(user, 'is_superuser', None)
-        #return is_member(user, 'admin') or is_superuser or is_superuser is None
-        return True
+        is_superuser = getattr(user, 'is_superuser', None)
+        print('is_superuser', is_superuser)
+        print('is admin member', is_member(user, 'admin'))
+        return is_member(user, 'admin') or is_superuser or is_superuser is None
     
 class AllowStaffGroupUsersModelBackend(ModelBackend):
     def user_can_authenticate(self, user):
-        #is_superuser = getattr(user, 'is_superuser', None)
-        #return is_member(user, 'staff') or is_superuser or is_superuser is None
-        return True
+        is_superuser = getattr(user, 'is_superuser', None)
+        print('is_superuser', is_superuser)
+        print('is staff member', is_member(user, 'staff'))
+        return is_member(user, 'staff') or is_superuser or is_superuser is None
