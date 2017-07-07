4e12b09a117b740b8c8be7f57b47f0517c6fd32b
bin wu <ben.w.dbm@gmail.com>
2017-07-07 10:39:14 +0800

    login required

--- apps/orders/views_new.py
+++ apps/orders/views_new.py
@@ -7,9 +7,11 @@ from common.utilities.views import ObjectListView
 from .models import Orders
 from . import filters
 from . import tables
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
-
+@method_decorator(login_required, name='dispatch')
 class OrderListView(ObjectListView):
     
     queryset = Orders.objects.all()
